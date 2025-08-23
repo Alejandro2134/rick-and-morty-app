73f04155f149e6fbd9c3d96df363249ee29a924a
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2025-08-23 14:36:06 -0500

    feat: graphql character types, resolvers and mutations

--- backend/src/infrastructure/web/frameworks/graphql/resolvers/CharacterResolver.ts
+++ backend/src/infrastructure/web/frameworks/graphql/resolvers/CharacterResolver.ts
@@ -1,10 +1,68 @@
-import { Query, Resolver } from 'type-graphql';
-import { Character } from '../types/Character';
+import { Query, Resolver, Arg, Int, Mutation } from 'type-graphql';
+import {
+    Character,
+    CharacterFilters,
+    CharacterUpdates,
+} from '../types/Character';
+import { CharacterRepoImpl } from '@infrastructure/persistency/orm/sequelize/implementations/CharacterRepoImpl';
+import { GetCharacters } from '@application/use_cases/character/GetCharacters';
+import { Service } from 'typedi';
+import { CharacterMapper } from '@application/mappers/CharacterMapper';
+import { GetCharacter } from '@application/use_cases/character/GetCharacter';
+import { UpdateCharacter } from '@application/use_cases/character/UpdateCharacter';
+import { CharacterCacheRepoImpl } from '@infrastructure/persistency/cache/redis/implementations/CharacterCacheRepoImpl';
+import { SecurityUtils } from '@infrastructure/utils/SecurityUtils';
 
+@Service()
 @Resolver()
 export class CharacterResolver {
+    private readonly characterMapper = new CharacterMapper();
+    private readonly getCharacters: GetCharacters;
+    private readonly getCharacter: GetCharacter;
+    private readonly updateCharacter: UpdateCharacter;
+
+    constructor(
+        private readonly characterRepoImpl: CharacterRepoImpl,
+        private readonly characterCacheRepoImpl: CharacterCacheRepoImpl,
+        private readonly securityUils: SecurityUtils,
+    ) {
+        this.getCharacters = new GetCharacters(
+            characterRepoImpl,
+            securityUils,
+            characterCacheRepoImpl,
+        );
+        this.getCharacter = new GetCharacter(
+            characterRepoImpl,
+            characterCacheRepoImpl,
+        );
+        this.updateCharacter = new UpdateCharacter(
+            characterRepoImpl,
+            characterCacheRepoImpl,
+        );
+    }
+
     @Query((_returns) => [Character])
-    async characters() {
-        return [];
+    async characters(
+        @Arg('filters', { nullable: false }) filters: CharacterFilters,
+    ) {
+        const res = await this.getCharacters.execute(filters);
+        return res.map(this.characterMapper.fromDomainToDTO);
+    }
+
+    @Query((_returns) => Character)
+    async character(@Arg('id', () => Int, { nullable: false }) id: number) {
+        const res = await this.getCharacter.execute(id);
+        return this.characterMapper.fromDomainToDTO(res);
+    }
+
+    @Mutation((_returns) => Character)
+    async update(
+        @Arg('id', () => Int, { nullable: false }) id: number,
+        @Arg('updates', { nullable: false }) updates: CharacterUpdates,
+    ) {
+        const mappedUpdates =
+            this.characterMapper.fromPartialDTOToPartialDomain(updates);
+        const res = await this.updateCharacter.execute(id, mappedUpdates);
+        return this.characterMapper.fromDomainToDTO(res);
     }
 }
--- backend/src/infrastructure/web/frameworks/graphql/types/Character.ts
+++ backend/src/infrastructure/web/frameworks/graphql/types/Character.ts
@@ -1,4 +1,11 @@
-import { Field, ID, ObjectType } from 'type-graphql';
+import {
+    Field,
+    ID,
+    InputType,
+    ObjectType,
+    registerEnumType,
+} from 'type-graphql';
+import { Comment } from './Comment';
 
 @ObjectType()
 export class Character {
@@ -22,4 +29,61 @@ export class Character {
 
     @Field()
     image!: string;
+
+    @Field()
+    is_favorite!: boolean;
+
+    @Field(() => [Comment])
+    comments!: Comment[];
+}
+
+enum CharacterStatus {
+    Alive = 'Alive',
+    Dead = 'Dead',
+    Unknown = 'unknown',
+}
+
+enum CharacterGender {
+    Female = 'Female',
+    Male = 'Male',
+    Genderless = 'Genderless',
+    Unknown = 'unknown',
+}
+
+registerEnumType(CharacterStatus, {
+    name: 'CharacterStatus',
+    description: 'The status of the character',
+});
+
+registerEnumType(CharacterGender, {
+    name: 'CharacterGender',
+    description: 'The gender of the character',
+});
+
+@InputType()
+export class CharacterFilters {
+    [filter: string]: string | undefined;
+
+    @Field(() => CharacterStatus, { nullable: true })
+    status?: CharacterStatus;
+
+    @Field({ nullable: true })
+    species?: string;
+
+    @Field(() => CharacterGender, { nullable: true })
+    gender?: CharacterGender;
+
+    @Field({ nullable: true })
+    name?: string;
+
+    @Field({ nullable: true })
+    origin?: string;
+}
+
+@InputType()
+export class CharacterUpdates {
+    [filter: string]: boolean | undefined;
+
+    @Field({ nullable: true })
+    is_favorite?: boolean;
 }
